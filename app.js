8b49845250b062c0e5a23032c16b12c47cbf43a2
Christian <leech92@gmail.com>
2022-01-17 02:47:33 -0500

    create skeleton and user auth

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+
+mongoose
+    .connect(db, {useNewURLParser: true})
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err))
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!!!");
+});
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {console.log(`Listening on port ${port}`)});
