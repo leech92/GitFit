63be847b284c5013ef1a9f6a8607a89c6236678e
tasangpo <tashi.rayshar@gmail.com>
2022-01-17 11:05:06 -0500

    front end prelim setup

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
