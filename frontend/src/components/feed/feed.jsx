08b3fe7c5eaf5fde17c2f80ae4569ff6ae6b1bb1
tasangpo <tashi.rayshar@gmail.com>
2022-01-18 12:48:22 -0500

    feed

---
+++ frontend/src/components/feed/feed.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Feed extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                Hello, this is the feed;
+            </div>
+        )
+    }
+}
