8f12e5e20541179a4a78b3007d34ec5d7b5ecb46
anna <annayth@gmail.com>
2022-01-18 10:37:51 -0500

    oh this user follows is struggles

--- frontend/src/components/navBar.js
+++ frontend/src/components/navBar.js
@@ -5,6 +5,10 @@ const NavBar = () => {
     return (
         <div>
             navBar
+            <p>Home</p>
+            <p>My Workouts</p>
+            <p>My Meal Plans</p>
+            <p>Profile</p>
         </div>
     )
 }
