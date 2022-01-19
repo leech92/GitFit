a55ee272b3718379decdd724f5d35ceb8908ae04
mcountryman <countryman.marco@gmail.com>
2022-01-18 20:46:21 -0500

    push to heroku:
    '
    
    :q
    exit

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const mealplans = require("./routes/api/mealplans");
 const meals = require("./routes/api/meals");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(___dirname, 'frontend', 'build', 'index.html'))
+    })
+}
 
 mongoose
     .connect(db, {useNewURLParser: true})
@@ -30,6 +38,6 @@ app.use("/api/users", users);
 app.use("/api/mealplans", mealplans);
 app.use("/api/meals", meals);
 
-const port = process.env.PORT || 4000;
 
+const port = process.env.PORT || 4000;
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
