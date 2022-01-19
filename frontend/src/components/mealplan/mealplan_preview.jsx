05abfb279a09323c973b79dd17ceb6eceab10dc0
Christian <leech92@gmail.com>
2022-01-19 17:40:14 -0500

    create mealplan links in profile

--- frontend/src/components/mealplan/mealplan_preview.jsx
+++ frontend/src/components/mealplan/mealplan_preview.jsx
@@ -7,6 +7,25 @@ class MealplanPreview extends React.Component {
     }
 
     render() {
-        
+        const { mealplans } = this.props;
+
+        return(
+            <div className="mealplan-pre-container">
+                <h1 className="mealplan-pre-head">My Meal Plans</h1>
+                <div>
+                {mealplans.map((mealplan,idx) => {
+                    return(
+                        <Link key={idx} className="mealplan-pre-link" to={`/mealplans/${mealplan._id}`}>
+                            <h1 className="mealplan-pre-name">{mealplan.name}</h1>
+                            <p className="mealplan-pre-type">Meal Plan Goal: {mealplan.mealplanType}</p>
+                            <p className="mealplan-pre-desc">{mealplan.description}</p>
+                        </Link>
+                    )
+                })}
+                </div>
+            </div>
+        )
     }
 }
+
+export default MealplanPreview;
