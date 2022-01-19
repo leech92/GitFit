2fe2cc85fc8022542539331c62cdc1ca9ab1861e
Christian <leech92@gmail.com>
2022-01-18 21:36:01 -0500

    complete backend routes for meal and mealplan

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const users = require("./routes/api/users");
 const mealplans = require("./routes/api/mealplans");
 const meals = require("./routes/api/meals");
-const bodyParser = require('body-parser');
-const passport = require('passport');
 
 mongoose
     .connect(db, {useNewURLParser: true})
     .then(() => console.log("Connected to mongoDB"))
     .catch(err => console.log(err))
+
+app.get("/", (req, res) => {
+    res.send("Hello World!!!");
+});
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
@@ -22,14 +26,9 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World!!!");
-});
-
 app.use("/api/users", users);
 app.use("/api/mealplans", mealplans);
 app.use("/api/meals", meals);
 
 const port = process.env.PORT || 4000;
-
 app.listen(port, () => {console.log(`Listening on port ${port}`)});
--- routes/api/mealplans.js
+++ routes/api/mealplans.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const passport = require('passport');
+const passport = require('passport');
 
 const Mealplan = require('../../models/Mealplan');
 
@@ -20,18 +20,44 @@ router.get('/:id', (req, res) => {
 router.post('/',
     // passport.authenticate('jwt', { session: false }), 
     (req, res) => {
-    const newMealplan = new Mealplan({
-        user: req.body.id,
-        name: req.body.name,
-        mealplanType: req.body.mealplanType,
-        calories: req.body.calories,
-        protein: req.body.protein,
-        carbs: req.body.carbs,
-        fat: req.body.fat,
-        description: req.body.description
+        const newMealplan = new Mealplan({
+            user: req.body.id,
+            name: req.body.name,
+            mealplanType: req.body.mealplanType,
+            calories: req.body.calories,
+            protein: req.body.protein,
+            carbs: req.body.carbs,
+            fat: req.body.fat,
+            description: req.body.description
     })
 
     newMealplan.save().then(mealplan => res.json(mealplan));
 });
+
+router.patch('/:id',
+    // passport.authenticate('jwt', { session: false }), 
+    (req, res) => {
+        Mealplan.findById(req.params.id)
+            .then(mealplan => {
+                mealplan.name = req.body.name
+                mealplan.mealplanType = req.body.mealplanType
+                mealplan.calories = req.body.calories
+                mealplan.protein = req.body.protein
+                mealplan.carbs = req.body.carbs
+                mealplan.fat = req.body.fat
+                mealplan.description = req.body.description
+
+                mealplan.save().then(() => res.json('Mealplan successfully updated!'))
+            })
+    }
+);
+
+router.delete('/:id',
+    // passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Mealplan.findByIdAndDelete(req.params.id)
+            .then(mealplan => res.json(mealplan))
+    }
+);
 
 module.exports = router;
