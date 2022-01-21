ead75b6c68b1e814e69fb7f59a24de26bffeaf64
Christian <leech92@gmail.com>
2022-01-21 01:04:35 -0500

    and more styling

--- frontend/src/components/mealplan/mealplan_preview.jsx
+++ frontend/src/components/mealplan/mealplan_preview.jsx
@@ -13,16 +13,18 @@ class MealplanPreview extends React.Component {
         return(
             <div className="mealplan-pre-container">
                 <h1 className="mealplan-pre-head">My Meal Plans</h1>
-                <div>
-                {mealplans.map((mealplan,idx) => {
-                    return(
-                        <Link key={idx} className="mealplan-pre-link" to={`/mealplans/${mealplan._id}`}>
-                            <h1 className="mealplan-pre-name">{mealplan.name}</h1>
-                            <p className="mealplan-pre-type">Meal Plan Goal: {mealplan.mealplanType}</p>
-                            <p className="mealplan-pre-desc">{mealplan.description}</p>
-                        </Link>
-                    )
-                })}
+                <div className="mealplan-pre-bot">
+                    {mealplans.map((mealplan,idx) => {
+                        return(
+                            <Link key={idx} className="mealplan-pre-link" to={`/mealplans/${mealplan._id}`}>
+                                <div className="mealplan-inner-link">
+                                    <h1 className="mealplan-pre-name">{mealplan.name}</h1>
+                                    <p className="mealplan-pre-type">Meal Plan Goal: {mealplan.mealplanType}</p>
+                                    <p className="mealplan-pre-desc">{mealplan.description}</p>
+                                </div>
+                            </Link>
+                        )
+                    })}
                 </div>
             </div>
         )
