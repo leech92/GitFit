6bb5ba7a515c685b44f8798a6db741048f1d3697
Christian <leech92@gmail.com>
2022-01-31 14:07:50 -0500

    continue to work on mealplan validations

--- routes/api/mealplans.js
+++ routes/api/mealplans.js
@@ -2,18 +2,21 @@ const express = require('express');
 const router = express.Router();
 // const passport = require('passport');
 const Mealplan = require('../../models/Mealplan');
+const validateMealplanInput = require('../../validation/mealplans')
 
 //for profile page mealplan preview
 router.get('/users/:user_id', (req, res) => {
     Mealplan.find({user: req.params.user_id})
         .sort({ calories: 1 })
         .then(mealplans => res.json(mealplans))
+        .catch(err => res.status(404).json({ nomealplansfound: 'No meal plans found from that user'}))
 });
 
 //for mealplan show page
 router.get('/:id', (req, res) => {
     Mealplan.findById(req.params.id)
         .then(mealplan => res.json(mealplan))
+        .catch(err => res.status(404).json({ nomealplansfound: 'No meal plan found with that ID'}))
 });
 
 //created for testing but will leave for possible use
@@ -24,6 +27,12 @@ router.get('/', (req, res) => {
 router.post('/',
     // passport.authenticate('jwt', { session: false }), 
     (req, res) => {
+        const { errors, isValid } = validateMealplanInput(req.body)
+
+        if (!isValid) {
+            return res.status(400).json(errors)
+        }
+
         const newMealplan = new Mealplan({
             user: req.body.user,
             name: req.body.name,
--- validation/mealplans.js
+++ validation/mealplans.js
@@ -11,7 +11,29 @@ module.exports = function validateMealplanInput(data) {
     data.protein = validNumber(data.protein) ? data.protein : 0;
     data.carbs = validNumber(data.carbs) ? data.carbs : 0;
     data.fat = validNumber(data.fat) ? data.fat : 0;
-    data.description = validText(data.description) ? data.description : 0;
+    data.description = validText(data.description) ? data.description : '';
 
-    if (!)
+    if (Validator.isEmpty(data.name)) {
+        errors.name = "Name is required"
+    }
+
+    if (Validator.isEmpty(data.mealplanType)) {
+        errors.mealplanType = "Meal plan type is required"
+    }
+
+    if (Validator.isEmpty(data.calories)) {
+        errors.calories = "Amount of calories is required"
+    }
+
+    if (Validator.isEmpty(data.protein)) {
+        errors.protein = "Amount of protein is required"
+    }
+
+    if (Validator.isEmpty(data.carbs)) {
+        errors.carbs = "Amount of carbs is required"
+    }
+
+    if (Validator.isEmpty(data.fat)) {
+        errors.fat = "Amount of fat is required"
+    }
 }
