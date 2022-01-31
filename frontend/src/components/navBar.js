f001cbbdb14ba950628ee37378f6c9f435ee3e60
anna <annayth@gmail.com>
2022-01-31 12:43:20 -0500

    styled header

--- frontend/src/components/navBar.js
+++ frontend/src/components/navBar.js
@@ -19,12 +19,36 @@ class TopNavBar extends React.Component {
 
     render() {
         return (
+
+            // <header className='top-nav'>
+            //     <p className='top-button-outter top-button'>
+            //         <NavLink className='top-button-home' to="/">Home</NavLink>
+            //     </p>
+                
+            //     <div className="top-right">
+            //         <p className='top-button-outter'> 
+            //             <NavLink className='top-button-profile top-button' to="/profile">Profile</NavLink>
+            //         </p>
+            //         <button onClick={this.props.logout}>Logout</button>
+            //     </div>
+                
+            // </header>
+
             <header className='top-nav'>
-                <p className='top-button-outter top-button'><NavLink className='top-button-home' to="/">Home</NavLink></p>
+                <div className='top-left'>
+                    <NavLink to="/">Home</NavLink>
+                </div>
                 
                 <div className="top-right">
-                    <p className='top-button-outter'><NavLink className='top-button-profile top-button' to="/profile">Profile</NavLink></p>
+
+                    <NavLink to="/profile">New Workout</NavLink>
+
+                    <NavLink to="/profile">New Meal Plan</NavLink>
+                    
+                    <NavLink to="/profile">Profile</NavLink>
+                 
                     <button onClick={this.props.logout}>Logout</button>
+
                 </div>
                 
             </header>
