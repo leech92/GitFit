f583963d1ca7633b891ddc078e5c4ccdfb29828d
mcountryman <countryman.marco@gmail.com>
2022-02-02 16:33:58 -0500

    workout show restyled

--- frontend/src/components/workouts/workout_show.jsx
+++ frontend/src/components/workouts/workout_show.jsx
@@ -49,11 +49,11 @@ class WorkoutShow extends React.Component {
                 <li className = "workout-show-item" key = {`exercise-${idx}`}>
                     <img src= {exercise.photoUrl} alt= "exercise" className = "workout-show-image"/>
                     <div className = "exercise-details">
-                        <span>{exercise.bodyPart}</span>
-                        <span>{exercise.name}</span>
-                        <span>{exercise.description}</span>
-                        <span>Target Reps: {exercise.targetRep}</span>
-                        <span>Sets: {exercise.sets}</span>
+                        <span className = "exercise-name" style={{"fontSize": "25px", "fontWeight": "bolder"}}>{exercise.name}</span>
+                        <span className = "exercise-additional">{exercise.bodyPart}</span>
+                        <span className = "exercise-additional">{exercise.description}</span>
+                        <span className = "exercise-additional">Target Reps: {exercise.targetRep}</span>
+                        <span className = "exercise-additional">Sets: {exercise.sets}</span>
                     </div>
                     
                     
@@ -62,10 +62,7 @@ class WorkoutShow extends React.Component {
         })
         return (
             <div className = "workout-show-container">
-                {/* <div className = "workout-show-video-container">
-                     <video src= "https://gitfit-app-images.s3.amazonaws.com/grind.mp4" autoPlay = {true} loop muted className = "workout-show-video"></video>
-                     <span className = "workout-grind">SLEEP.SWEAT.GRIND.REPEAT.</span>
-                </div> */}
+
                 <div className = "workout-show-title">
                     <span>Let's get it, time for some {this.props.workout.title}</span>
                 </div>
