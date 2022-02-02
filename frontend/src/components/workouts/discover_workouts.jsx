63c45fe081a78ec4e1cc2550c9b1daa7c3f15468
mcountryman <countryman.marco@gmail.com>
2022-02-02 14:49:19 -0500

    discover style done

--- frontend/src/components/workouts/discover_workouts.jsx
+++ frontend/src/components/workouts/discover_workouts.jsx
@@ -38,8 +38,6 @@ class DiscoverWorkouts extends React.Component {
             <div className = "discover-workout-container">
                 <div className = "workout-top-message">
 
-                    {/* <img src="https://gitfit-app-images.s3.amazonaws.com/gov-arnold.jpg" alt="arnold-motivation" className = "workout-img-arnold"/> */}
-                    {/* <video src= "https://gitfit-app-images.s3.amazonaws.com/motivation.mp4" autoPlay = {true} loop muted className = "workout-banner"></video> */}
                     <div className = "discover-workout-motivation">
                         <span className = "workout-header">GitFit Workouts</span>
                         <span className = "workout-message-content">Our workouts were created by the best, for the best. It's time to get it done. No excuses. </span>
